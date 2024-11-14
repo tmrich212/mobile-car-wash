059a012d688754b9a6f72ca6515656bf513e0fce
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2024-11-13 23:08:36 -0800

    tailwind added

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <header class="absolute inset-x-0 top-0 z-50 text-white">
+                <nav class="flex items-center justify-between p-6 lg:px-8 color-white" aria-label="Global">
+                <div class="flex lg:flex-1">
+                    <a href="#" class="-m-1.5 p-1.5">
+                    <h3>mobile car washing campos</h3>
+                    </a>
+                   </div> 
+                </nav>
+            </header>
+          </div>
+    )
+}
+
+export default Header;
