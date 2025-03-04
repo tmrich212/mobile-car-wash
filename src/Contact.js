b05f1e3bf799f11acba8bd48f0251d352eb2702a
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2025-03-04 13:25:54 -0800

    Calendly schedule widget added

--- src/Contact.js
+++ src/Contact.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import './Contact.css'
+import './Contact.css';
+import {PopupWidget} from 'react-calendly';
 
 function Contact () {
     return (
         <div className='Contact'>
-        <h2>Get in Touch</h2>
+            <h1>Appointments</h1>
+            <h3 className='appt'>Once you have scheduled a time for services, we will reach out to confirm your appointment.</h3>
 
-        <form target="_blank" action="https://formsubmit.co/tmrichards295@gmail.com" method="POST">
-            <div className="form-group">
-            <div className="form-row">
-                <div className="col">
-                <input type="text" className="fullName" class="form-control" placeholder="Full Name" required />
-                </div>
-                <div className="col">
-                <input type="email" className="email" class="form-control" placeholder="Email Address" required />
-                </div>
-            </div>
-            </div>
-            <div className="form-group">
-            <textarea placeholder="Your Message" class="form-control" className="message" rows="10" required></textarea>
-            </div>
-            <button type="submit" className="btn">Submit Form</button>
-        </form>
+        <PopupWidget
+        url="https://calendly.com/tmrichards295"
+        /*
+         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+         */
+        rootElement={document.getElementById("root")}
+        text="Schedule my appointment"
+        textColor="ffffff"
+        color="#00a2ff"
+      />
     </div>
     )
 }
